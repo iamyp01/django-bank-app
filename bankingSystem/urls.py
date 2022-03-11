681bc81df12d9f3cf7e16237e01c0c823ee69b0f
unknown <yuvrajparashar710@gmail.com>
2022-03-11 12:47:31 +0530

    c

--- bankingSystem/urls.py
+++ bankingSystem/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.static import serve
-from django.conf.urls.static import url
+# from django.conf.urls.static import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('', include('accounts.urls'))
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
